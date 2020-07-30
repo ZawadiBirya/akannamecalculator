4db9483e27765c2369c1d8718ba5b714c239208b
zawadi.birya <zawadibirya@gmail.com>
2020-07-30 08:20:24 +0300

    Adding logic to JS file

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-var femaleakannames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-var maleakannames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-var dayoftheweek = ['Sunday' , 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-var gender = document.getElementById("gender");
-var dob = document.getElementById("bday");
-dob = new Date(dob);
-weekday = dob.getDay();
+var femaleAkanNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+var maleAkanNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+var weekDays = ['Sunday' , 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-function Submitdata () {
-if (gender == female && weekday == dayoftheweek[0]) {
-document.getElementById("result").innerHTML = "Your Akan name is" + femaleakannames[0];
-}
+function submit() {
 event.preventDefault();
+var gender = document.getElementById("gender");
+var date = document.getElementById("date");
+date = new Date(date);
+weekDay = date.getDay();
+
+if (theGender === "male") {
+document.getElementById("result").innerHTML = "Your Akan name is" + maleAkanNames[date.getDay()];
+} else if (theGender === "female") {
+document.getElementById("result").innerHTML = "Your Akan name is" + femaleAkanNames[date.getDay()];
+} else {
+document.getElementById("result").innerHTML = "invalid";
+}
      }
